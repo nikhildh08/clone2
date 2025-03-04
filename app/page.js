ad4a97ce29f4dde561240a46b6f30ba9fd7f16ee
nikhildh08 <nikhil@dhwaniastro.com>
2025-03-04 18:55:16 +0530

    Add Home Preview Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-    </div>
+    <>
+      <Head>
+        <title>My Project</title>
+        <meta property="og:title" content="Dhwani Astro Dashboard" />
+        <meta property="og:description" content="Astrologer Panel/Dashboard" />
+        {/* <meta property="og:image" content="https://yourdomain.com/preview-image.jpg" />
+        <meta property="og:url" content="https://yourdomain.com" /> */}
+        {/* <meta name="twitter:card" content="" /> */}
+      </Head>
+      {/* <h1>Welcome back to Dhwani Astro</h1> */}
+    </>
   );
 }
